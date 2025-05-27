d863fd6dd79591e294c2694c854ee11fe9c652c7
SongJongbeen <1041489@gmail.com>
2025-05-27 21:04:02 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+    await bot.load_extension('cogs.qa')  # Cog 로딩
+    await bot.load_extension('cogs.search_member')
+    await bot.load_extension('cogs.check_promotion')
+    await bot.load_extension('cogs.manage_role')
+    await bot.load_extension('cogs.ladder_game')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run(os.getenv('DISCORD_TOKEN'))
